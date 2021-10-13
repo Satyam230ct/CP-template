77cc464ad710f647d2af3986aee48d05bd995880
Satyam Kumar <63579695+yaduvanshii092@users.noreply.github.com>
2021-10-13 13:21:29 +0530

    Phi Euclear Fermat Wilson GDC

--- Number Theory.cpp
+++ Number Theory.cpp
@@ -39,6 +39,7 @@ inv[i]=inverse of ppi[i] w.r.t num[i]
 Euler’s Phi function (also known as totient function, a function on natural numbers
 that gives the count of positive integers coprime with the corresponding natural number.
 */
+
 int phi[] = new int[n+1];
 void fill()
 {
@@ -50,6 +51,28 @@ void fill()
     phi[i] = i; //phi[1] is 0
 }
 
+=> if n is a positive integer and a, n are coprime, then aφ(n)+1 ≡ a mod n.
+
+// Application-> Of Phi WIth GCD
+
+Q-> You are given two integers a and m. (1≤a<m≤10^(10)).
+Calculate the number of integers x such that 0≤x<m and gcd(a,m)=gcd(a+x,m).
+
+Solution->
+
+The Euclidean algorithm is based on the next fact: if a≥b then gcd(a,b)=gcd(a−b,b). 
+So, if (a+x)≥m then gcd(a+x,m)=gcd(a+x−m,m). So we can declare that we are looking at 
+m different integers x′=(a+x)modm with 0≤ x′<m, so all x′ forms a segment [0,m−1].
+So, we need to find the number of x′ (0≤x′<m) such that gcd(x′,m)=gcd(a,m).
+
+Let's denote g=gcd(a,m), then a=ga′ and m=gm′. 
+So, gcd(a,m)=gcd(ga′,gm′)=g⋅gcd(a′,m′)=g or gcd(a′,m′)=1.
+Since gcd(x′,m)=gcd(a,m)=g so we also can represent x′=x′′g and, therefore gcd(x′′,m′)=1.
+
+Since 0≤x′<m, then 0≤x′′<m′ or we need to calaculate the number of x′′ (0≤x′′<m′) 
+such that gcd(x′′,m′)=1. Since gcd(0,m′)=m′>1 so we can consider x′′∈[1,m′−1] 
+and this is the definition of Euler's totient function φ(m′) which is the answer.
+
 //----------------------Wilson’s Theorem----------------------
 
 If p is a prime, then (p — 1)! = –1 (mod p)
@@ -57,6 +80,5 @@ If p is a prime, then (p — 1)! = –1 (mod p)
 //--------------------Fermat’s little Theorem---------------
 //   if p is a prime number,
   a^p == a(mod p)
-  
   
   
