76a47d3059f2864b20cecc434e783bc0a6e8cd28
Satyam Kumar <63579695+yaduvanshii092@users.noreply.github.com>
2021-10-14 19:58:04 +0530

    Better for non continuous Jumps problem to do colouring. O(n)

--- Disjoint set union.cpp
+++ Disjoint set union.cpp
@@ -1,16 +1,17 @@
 //-----------------DSU----------------------------------------
-
-class dsu{
+class dsu{  
+public:
     int v;
     int *parent;
-public:
+//     int *siz;
     dsu(int n)
     {
         this->v=n;
         parent=new int[v];
+//         siz=new int[v]; for(int i=0;i<n;i++)siz[i]=1;
         iota(parent,parent+n,0);  // Assiging p[i]=i;
+        
     }
-
     int get(int x)
     {
         return (x==parent[x])?x:(parent[x]=get(parent[x]));
@@ -18,11 +19,21 @@ public:
     void unite(int x,int y)
     {
         x=get(x);   y=get(y);
+        
+        // Use Cycle Detection Only
+//         /*
         if(x!=y)
         {
             parent[x]=max(parent[x],parent[y]);
             parent[y]=max(parent[x],parent[y]);
         }
+//         */
+
+        // Use When We have to deal With Size also 
+        /*
+        if(siz[x]<siz[y])swap(x,y);
+        siz[x]+=siz[y]; parent[y]=x;
+        */
     }
 };
 
