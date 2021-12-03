092d97e2a90073d5c93894aa83a2a5222466c4fc
Satyam Kumar <63579695+yaduvanshii092@users.noreply.github.com>
2021-12-03 21:27:56 +0530

    Update Prime Number Sieve.cpp

--- Prime Number Sieve.cpp
+++ Prime Number Sieve.cpp
@@ -18,7 +18,7 @@ void primes() {
 
 // 2) Prime in O(NloglogN) 1e7 range
 
-const int N8=10000000;
+const int N7=10000000;
 bitset<10000007> b;
 
 void primes()
@@ -27,26 +27,27 @@ void primes()
     b.set();    //1,1,1,1,1,.....
     b[0]=b[1]=0;
     //Even Elemenator
-    for(ll i=4;i<=N8;i+=2)
+    prime.push_back(2);
+    for(int i=4;i<=N7;i+=2)
     b[i]=0;
 
-    for(ll i=3;i<=N8;i+=2)
+    for(int i=3;i<=N7;i+=2)
     if(b[i])
     {
         prime.push_back(i);
-        for(ll j=i*i;j<=N8;j+=i)
+        for(int j=i*i;j<=N7;j+=i)
         b[j]=0;
     }
 }
 
 // 3) Segment Seave
 int l,r;
-ll segment[("range")];
+int segment[("range")];
 for(auto x:prime)
 {
     if(x*x>n)break;
 
-    ll start=(m/x)*x;
+    int start=(m/x)*x;
     if(x>=m and x<=n)
     {
         start=2*x;
@@ -57,27 +58,30 @@ for(auto x:prime)
     for(i=start;i<=n;i+=x)
     segment[i-m]=1;    
 }
+
 // ----------------------------------------------------------------------
 // Factorization of a Number using this Sieve :
-vector <ll> factorize(ll m)
+vector <int> factorize(int m)
 {
-    vector <ll> factors;
+    vector <int> factors;
     factors.clear();
-    ll i = 0;
-    ll p = primes[i];
+    int i = 0;
+    int p = prime[i];
     while(p*p <= m)
     {
         if(m%p == 0)
         {
-            factors.pb(p);
+            factors.push_back(p);
             while(m%p == 0)
-                m = m/p;
+            {
+                 m = m/p;
             }
-                 i++;
-               p = primes[i];
+        }
+        i++;
+        p = prime[i];
     }
     if(m!=1)
-    factors.pb(m);
+    factors.push_back(m);
     return factors;
 }
 
