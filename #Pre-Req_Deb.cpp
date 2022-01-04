1c328e9ae1335e33665ce09cf15cb24eadd52a49
Satyam Kumar <63579695+yaduvanshii092@users.noreply.github.com>
2022-01-05 01:52:38 +0530

    Create #Pre-Req_Deb.cpp

---
+++ #Pre-Req_Deb.cpp
@@ -0,0 +1,53 @@
+const int d4i[4]={-1, 0, 1, 0}, d4j[4]={0, 1, 0, -1};
+const int d8i[8]={-1, -1, 0, 1, 1, 1, 0, -1}, d8j[8]={0, 1, 1, 1, 0, -1, -1, -1};
+
+int rint(int  l , int r){return rand()%(r-l+1) + l ;}
+
+mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
+ll randint(ll a, ll b) {
+	return uniform_int_distribution<ll>(a, b)(mt_rng);
+}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const uint64_t SEED = chrono::steady_clock::now().time_since_epoch().count() * (random_address() | 1);
+mt19937_64 rng(SEED);
+
+
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// --------------------------Dont Know Now------------------------------
+
+typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtr
+
+//------------------------------Debuger----------------------------
+void DBG() {
+	cerr << "]" << endl;
+}
+template<class H, class... T> void DBG(H h, T... t) {
+	cerr << to_string(h);
+	if(sizeof...(t))
+		cerr << ", ";
+	DBG(t...);
+}
+#ifdef _DEBUG
+#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#else
+#define dbg(...) 0
+#endif
+//  -------------------------------------
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " : " << arg1 << std::endl;
+        //use cerr if u want to display at the bottom
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ','); cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+    }
+#else
+#define trace(...)
+#endif
