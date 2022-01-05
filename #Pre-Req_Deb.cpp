3182c50ddc54e0c8f5901eef9c3fd396b6093afe
Satyam Kumar <63579695+yaduvanshii092@users.noreply.github.com>
2022-01-05 16:16:08 +0530

    Completed

--- #Pre-Req_Deb.cpp
+++ #Pre-Req_Deb.cpp
@@ -1,53 +1,53 @@
+//-----------------------------Debugger----------------------------------------
+#define START_FUCK
+#ifdef START_FUCK
+#define deb(x) cerr<<(#x)<<"= ";_print(x);cerr<<'\n';
+#else
+#define deb(x)
+#endif
+void _print(const int t) {cerr << t;}
+void _print(const string t) {cerr << t;}
+void _print(const char t) {cerr << t;}
+void _print(const double t) {cerr << t;}
+template <class T, class V> void _print(const pair<T,V> &p)
+{cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(const vector<T> &v) 
+{cerr<<"[ "; for(T i : v){_print(i); cerr << " ";}cerr << "]";}
+template <class T> void _print(const set<T> &v) 
+{cerr << "[ "; for(T i : v){_print(i); cerr << " ";} cerr << "]";}
+template<class T> void _print(const multiset<T> &v) 
+{cerr << "[ "; for(T i : v){_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(const map<T,V> &v) 
+{cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//-------------------------------end_deb---------------------------------------
+//------------------------Generate Sample Test Cases---------------------------
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+vector<int> get_horney(int n,int l=1,int h=10){int range_cover=h-l+1;
+vi res(n); fr(i,n)res[i]=l+rng()%range_cover;  return res;}
+
+// -----------------------------Testing----------------------------------------
+int brute_solution(){
+    int ans=0;
+    return ans;
+}
+int mine_solution(){
+    int ans=0;
+    return ans;
+}
+bool testing(){
+    int sexed=100;
+    while (sexed--){
+        int ans_brute=brute_solution();
+        int ans_mine=mine_solution();
+        if(ans_brute!=ans_mine)return false; }
+    return true;
+}
+//-----------------------------------------------------------------------------
+
 const int d4i[4]={-1, 0, 1, 0}, d4j[4]={0, 1, 0, -1};
 const int d8i[8]={-1, -1, 0, 1, 1, 1, 0, -1}, d8j[8]={0, 1, 1, 1, 0, -1, -1, -1};
 
-int rint(int  l , int r){return rand()%(r-l+1) + l ;}
-
-mt19937 mt_rng(chrono::steady_clock::now().time_since_epoch().count());
-ll randint(ll a, ll b) {
-	return uniform_int_distribution<ll>(a, b)(mt_rng);
-}
-
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-const uint64_t SEED = chrono::steady_clock::now().time_since_epoch().count() * (random_address() | 1);
-mt19937_64 rng(SEED);
-
-
-mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
-
 // --------------------------Dont Know Now------------------------------
-
 typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtr
+// ---------------------------------------------------------------------
 
-//------------------------------Debuger----------------------------
-void DBG() {
-	cerr << "]" << endl;
-}
-template<class H, class... T> void DBG(H h, T... t) {
-	cerr << to_string(h);
-	if(sizeof...(t))
-		cerr << ", ";
-	DBG(t...);
-}
-#ifdef _DEBUG
-#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
-#else
-#define dbg(...) 0
-#endif
-//  -------------------------------------
-#define TRACE
-#ifdef TRACE
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-    template <typename Arg1>
-    void __f(const char* name, Arg1&& arg1){
-        cout << name << " : " << arg1 << std::endl;
-        //use cerr if u want to display at the bottom
-    }
-    template <typename Arg1, typename... Args>
-    void __f(const char* names, Arg1&& arg1, Args&&... args){
-        const char* comma = strchr(names + 1, ','); cout.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
-    }
-#else
-#define trace(...)
-#endif
