185a49809452c10b888d4ef67d645d1837551fbe
Satyam Kumar <63579695+Satyam23Oct@users.noreply.github.com>
2022-02-24 10:20:22 +0530

    Update Number Theory.cpp

--- Number Theory.cpp
+++ Number Theory.cpp
@@ -22,6 +22,36 @@ ll extendedEuclidMethod(ll a,ll b)
 // If gcd(a,b)==1 then inverse of a Exist wrt b
 inverse=(extendedEuclidMethod(a,b)+b)%b;
 
+
+// Mathematical functions
+int GCD(int a, int b)
+{
+	while (b)
+	{
+		a %= b;
+		swap(a, b);
+	}
+	return a;
+}
+ 
+int GCD_extended(int a, int b, int &x, int &y)
+{
+	x = 1, y = 0;
+	int x1 = 0, y1 = 1, a1 = a, b1 = b;
+	while (b1)
+	{
+		int q = a1 / b1;
+		tie(x, x1) = make_tuple(x1, x - q * x1);
+		tie(y, y1) = make_tuple(y1, y - q * y1);
+		tie(a1, b1) = make_tuple(b1, a1 - q * b1);
+	}
+	return a1;
+}
+int LCM(int a, int b)
+{
+	return ((ll)a * b) / GCD(a, b);
+}
+ 
 //--- -------------------------Chinese Remainder Theorem---------------------------------
 
 x= Summation (rem[i]*pp[i]*inv[i])) % prod
