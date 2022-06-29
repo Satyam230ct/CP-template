daff72c6fc86cf9cb2a1e47205b8c865cb638683
Satyam Kumar <63579695+Satyam230ct@users.noreply.github.com>
2022-06-29 08:05:30 +0530

    Add files via upload

---
+++ String-Algos/Z-algo.cpp
@@ -0,0 +1,34 @@
+vector<int> z_algo(string const &s)
+{
+    int n=(int)s.size(),l=0,r=0;    
+    vector<int> z(n,0);
+
+    for(int i=1;i<n;i++){
+        //Condition 1
+        if(i>r){
+            l=r=i;
+            while (r<n and s[r-l]==s[r])r++;    
+            r--; // To miss match last r fromt the loop
+            z[i]=r-i+1;
+        }
+        else{   // Condition 2 Else i<=r
+            int j=i-l; // Current index in the previous match
+            if(z[j]<r-i+1)z[i]=z[j];
+            else{
+                // The vale of z[i] can be more now
+                l=i;   // Now we already know r-l+1 already matching search for more
+                while(r<n and s[r-l]==s[r])r++;
+                r--; 
+            }
+        }
+    }
+    return z;
+}
+
+void Shiddat(int Case)
+{
+    string s="abc$abcdabcfabdabe";
+    vi v=z_algo(s);
+    for(int i=0;i<sz(v);i++)cout<<v[i]<<" ";  // Before $ is pattern that have to matched
+    cout<<endl;
+}
